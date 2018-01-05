bf47ffc97784ba3c14c51714b898c7887e4a6e7c
CBWeekijP <weekij.p@cubiccreative.org>
2018-01-05 10:15:18 +0700

    ADD server helper and test

---
+++ server.js
@@ -0,0 +1,95 @@
+const http = require('http')
+const SocketIO = require('socket.io')
+const debug = require('debug')('socketio-test:server')
+
+/**
+ * Promise of result from server attempting to listen to a port
+ * @param {http.Server} server
+ * @param {int} port - port to listen to
+ * @return {Promise} resolve to port number if available, reject with error otherwise
+ */
+const listenPromise = (server, port) =>
+  new Promise((resolve, reject) => {
+    server
+      .once('error', reject)
+      .once('listening', () => resolve(port))
+      .listen(port)
+  })
+
+/**
+ * Attempt to make given server listen to a port in given range
+ * @param {http.Server} server
+ * @param {int} maxPort - largest port number to attempt listening to
+ * @param {int} basePort - port number to start attempting
+ * @return {int} port number that the server successfully listen to
+ * @throws {Error} attempt exceed max port
+ */
+const tryPort = async (server, maxPort, basePort) => {
+  let currentPort = basePort
+  while (currentPort <= maxPort) {
+    debug(`attempt to occupy port ${currentPort}`)
+    try {
+      const port = await listenPromise(server, currentPort)
+      return port
+    } catch (error) {
+      if (error.code === 'EADDRINUSE') {
+        debug(`port ${currentPort} is in-use, trying next port`)
+        currentPort += 1
+      } else {
+        debug(`error listening to port ${currentPort}: ${error.message}`)
+        throw error
+      }
+    }
+  }
+  debug(`max attempt reached at port ${maxPort}`)
+  throw new Error(`max attempt reached at port ${maxPort}`)
+}
+
+const extensionMethods = {
+  async start () {
+    const port = await tryPort(this.httpServer, this.maxPort, this.basePort)
+    this.endpoint = `http://localhost:${port}`
+    debug(`server is listening on port ${port}`)
+  },
+  stop () {
+    return new Promise((resolve, reject) => {
+      const address = this.httpServer.address()
+      if (address) {
+        this.httpServer.close(() => {
+          this.endpoint = null
+          debug(`server stopped listening on port ${address.port}`)
+          return resolve()
+        })
+      } else {
+        debug(`server is already closed`)
+        this.endpoint = null
+        return resolve()
+      }
+    })
+  }
+}
+
+/**
+ * Create Socket.io server for testing
+ * @param {object} options
+ * @param {int} options.basePort - base port to start attempting to listen, default to 3000
+ * @param {int} options.maxAttempt - number of port number increment attempt, default to 1000
+ * @return {IO.Server}
+ */
+module.exports = (options = {}) => {
+  // Settings
+  const basePort = options.basePort || 3000
+  const maxAttempt = options.maxAttempt || 1000
+  const maxPort = basePort + maxAttempt - 1
+
+  // Create HTTP and IO server
+  const server = http.createServer()
+  const io = SocketIO(server)
+
+  // Initialize endpoint property
+  io.endpoint = null
+  io.basePort = basePort
+  io.maxPort = maxPort
+
+  return Object.assign(io, extensionMethods)
+}
---
+++ server.test.js
@@ -0,0 +1,50 @@
+/* eslint-env jest */
+const Server = require('./server')
+
+describe('server helper', () => {
+  it('attempts to find available port and attach server on', async () => {
+    const settings = { basePort: 4000, maxAttempt: 10 }
+    const s1 = Server(settings)
+    const s2 = Server(settings)
+    const s3 = Server(settings)
+
+    try {
+      await s1.start()
+      await s2.start()
+      await s3.start()
+
+      expect(s1.endpoint).toBe('http://localhost:4000')
+      expect(s2.endpoint).toBe('http://localhost:4001')
+      expect(s3.endpoint).toBe('http://localhost:4002')
+
+      expect(s1.httpServer.address().port).toBe(4000)
+      expect(s2.httpServer.address().port).toBe(4001)
+      expect(s3.httpServer.address().port).toBe(4002)
+    } catch (error) {
+      throw error
+    } finally {
+      await s1.stop()
+      await s2.stop()
+      await s3.stop()
+    }
+  })
+  it('throws if attempt exceeds maximum value', async () => {
+    const settings = { basePort: 4000, maxAttempt: 2 }
+    const s1 = Server(settings)
+    const s2 = Server(settings)
+    const s3 = Server(settings)
+
+    expect.assertions(1)
+    try {
+      await s1.start()
+      await s2.start()
+      await s3.start()
+    } catch (error) {
+      expect(error.message).toBe('max attempt reached at port 4001')
+    } finally {
+      await s1.stop()
+      await s2.stop()
+      await s3.stop()
+    }
+  })
+})
